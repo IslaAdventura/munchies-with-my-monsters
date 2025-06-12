9e8614861161b40aba3e964d8fff9813009bdea3
Stephanie Anderson <srglover126@gmail.com>
2025-06-12 16:37:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
+          <h2>Ghouls Trip Creepy Assistant</h2>
+          <p>🖤✨🦇🦇🦇🦇🦇🦇🦇🦇🦇🦇🦇✨🖤</p>
+          <h3>At Your Service</h3>
           <Weather />
           <FoodReimbursement />
         </header>
